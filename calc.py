6d0cb7fc5e7413a61b264df2c59481d574af0987
sgoodm <sgoodman@aiddata.org>
2015-02-13 16:46:47 -0500

    Add new no_data object to builder_data.json based on percentage of projects with NODATA value (from raster, checked in calc.py).

--- calc.py
+++ calc.py
@@ -26,10 +26,13 @@ import numpy
 #sys.argv[0] is file name sent to python
 
 in_file = sys.argv[1] # file - csv file containing results from Point Extraction Tool
-in_type = sys.argv[2] # sector - type of aid the locations in "in_file" represent
-in_country = sys.argv[3] # country - country the aid in "in_file" is associated with
+in_country = sys.argv[2] # country - country the aid in "in_file" is associated with
+in_sector = sys.argv[3] # sector - type of aid the locations in "in_file" represent
 in_field =  sys.argv[4] # aid field name - column name aid is located in
 in_count = sys.argv[5] # count field name - column name that project count is located in
+
+# projects that have more than this many (as a percentage) projects matching the rasters NODATA value will be given a "no_data" flag in builder_data.json
+no_data_limit = 0.25
 
 # output file
 in_out = "/var/www/html/aiddata/data/form/form_data.json"
@@ -40,11 +43,21 @@ builder_data_handle = open('/var/www/html/aiddata/data/form/builder_data.json', 
 # load raster data from json
 builder_data = json.load(builder_data_handle)
 
+try:
+	nd_check = builder_data['no_data'][in_country]
+except:
+	builder_data['no_data'][in_country] = {}
+
+try:
+	nd_check = builder_data['no_data'][in_country][in_sector]
+except:
+	builder_data['no_data'][in_country][in_sector] = {}
+
 
 # general use blank data
 # clear Global->type before it is recalculated using new input data
 blank_json_data = {
-	"type": in_type,
+	"type": in_sector,
 	"projects": 0,
 	"total": 0
 }
@@ -99,6 +112,8 @@ for i in range(0, len(builder_data['raster_data'])):
 			"sd": 0,
 			"quart": 0,
 			"mean": 0,
+			"count": 0,
+			"nodata":0,
 			"thresh": 0
 		}
 
@@ -134,9 +149,13 @@ for i in range(0, len(builder_data['raster_data'])):
 			except:
 				aid = 0
 
+			sd['count'] += 1
 			if i == 0:
 				json_data["projects"] += 1
 				json_data["total"] += aid
+			
+			if float(row[folder]) == float(nodata):
+				sd['nodata'] += 1
 
 			if builder_data['raster_data'][i]['stats']['op'] == 'lte' and float(row[folder]) <= builder_data['raster_data'][i]['stats']['thresh']:
 				json_data["tot_"+name] += aid
@@ -147,18 +166,21 @@ for i in range(0, len(builder_data['raster_data'])):
 
 		csv_handle.close()
 
-		# json_data["total"] = json_data["total"]
-		# json_data["tot_"+name] = json_data["tot_"+name]
 		json_data["per_"+name] = 100 * json_data["tot_"+name] / json_data["total"]
+
+		# check nodata vs count
+		builder_data['no_data'][in_country][in_sector][name] = False
+		if float(sd['nodata']) / float(sd['count']) > no_data_limit:
+			builder_data['no_data'][in_country][in_sector][name] = True
 
 
 
 builder_data_handle.close()
 
 # dump json back into builder_data.json if any thresh values were updated
-if update_builder_data == True:
-	with open('/var/www/html/aiddata/data/form/builder_data.json', 'w') as temp_handle:
-		json.dump(builder_data, temp_handle, sort_keys = True, indent = 4, ensure_ascii=False)
+# if update_builder_data == True:
+with open('/var/www/html/aiddata/data/form/builder_data.json', 'w') as temp_handle:
+	json.dump(builder_data, temp_handle, sort_keys = True, indent = 4, ensure_ascii=False)
 
 
 # open json for reading
@@ -182,7 +204,7 @@ with open(in_out, 'r') as json_handle:
 		json_full[in_country] = {"Total":{},"Agriculture":{},"Education":{},"Health":{},"Industry":{},"Water":{},"Other":{}}
 
 	# replace input country>type with new json_data
-	json_full[in_country][in_type] = json_data
+	json_full[in_country][in_sector] = json_data
 
 
 	# ----- UPDATE COUNTRY > TOTAL
@@ -211,18 +233,18 @@ with open(in_out, 'r') as json_handle:
 	# ----- UPDATE Global > TOTAL
 
 	# clear "Global">type amnd "Global">"Total" before it is recalculated using new input data
-	json_full["Global"][in_type] = blank_json_data.copy()
+	json_full["Global"][in_sector] = blank_json_data.copy()
 	json_full["Global"]["Total"] = blank_json_data.copy()
 	json_full["Global"]["Total"]["type"] = "Total"
 
 	# update Global data
 	for j_country in json_full:
-		if j_country != "Global" and in_type in json_full[j_country]:
+		if j_country != "Global" and in_sector in json_full[j_country]:
 	
 			# update "Global">type
-			for j_data in json_full[j_country][in_type]:
+			for j_data in json_full[j_country][in_sector]:
 				if j_data != "type":
-					json_full["Global"][in_type][j_data] += json_full[j_country][in_type][j_data]
+					json_full["Global"][in_sector][j_data] += json_full[j_country][in_sector][j_data]
 			
 			# update "Global">"Total"
 			if "Total" in json_full[j_country]:
@@ -232,7 +254,7 @@ with open(in_out, 'r') as json_handle:
 
 	# calculate "Global" percentages (above update took sum of all fields)
 	for cat in categories:
-		json_full["Global"][in_type]["per_"+cat] = 100 * json_full["Global"][in_type]["tot_"+cat] / json_full["Global"][in_type]["total"]
+		json_full["Global"][in_sector]["per_"+cat] = 100 * json_full["Global"][in_sector]["tot_"+cat] / json_full["Global"][in_sector]["total"]
 		json_full["Global"]["Total"]["per_"+cat] = 100 * json_full["Global"]["Total"]["tot_"+cat] / json_full["Global"]["Total"]["total"]
 
 
@@ -243,9 +265,9 @@ with open(in_out, 'r') as json_handle:
 			if j_data != "type":
 				json_full[x][y][j_data] = round(json_full[x][y][j_data], 2)
 
-	rounder(in_country, in_type)
+	rounder(in_country, in_sector)
 	rounder(in_country, "Total")
-	rounder("Global", in_type)
+	rounder("Global", in_sector)
 	rounder("Global", "Total")
 
 
